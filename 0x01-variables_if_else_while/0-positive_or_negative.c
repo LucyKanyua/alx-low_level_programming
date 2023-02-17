29d96f085188f4074b886989085e55e9684642d7
LucyKanyua <lucy.munyange@gmail.com>
2023-02-17 03:49:34 -0800

    posneg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,9 @@
 
 /*
  * main - prints if the number is positive, negative  or 0
+ *
  * descripion - if/else statement declares whether a number is positive, negative or zero
+ *
  * Return: Always 0
  */
 int main(void)
