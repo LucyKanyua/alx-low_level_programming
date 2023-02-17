2353eeeb5edcdf09d34038e7b8fbae2b4645f8c9
LucyKanyua <lucy.munyange@gmail.com>
2023-02-17 11:45:15 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints single digits in base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 1o; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
