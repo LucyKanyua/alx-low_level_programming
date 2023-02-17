902437b98abd65b48298aa236a18575638ad406f
LucyKanyua <lucy.munyange@gmail.com>
2023-02-17 10:39:19 -0800

    print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,9 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch)
-	for (ch = 'A'' ch <= 'Z'; ch++)
+			
+	for (ch = 'A'; ch <= 'Z'; ch++)
+
 	putchar('\n');
 
 	return (0);
