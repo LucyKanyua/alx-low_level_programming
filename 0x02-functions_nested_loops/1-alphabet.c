837ede00f87e989cedaac27c3605d4e763f82785
LucyKanyua <lucy.munyange@gmail.com>
2023-02-21 07:50:02 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints alphabets in lowercase
+ * description - prints lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	char la;
+
+	for (la 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n');
+}
