4ee9e7db26acda7fab7133ad3ff81a546e4c19d0
LucyKanyua <lucy.munyange@gmail.com>
2023-02-21 22:43:59 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,20 +3,21 @@
 /**
  * print_to_98 - prints all numbers from input to 98
  * @n: the starting number
- * Return: Always 0
+ * Return: 0 or 1
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	while (n < 98)
 	{
+		printf("%i, ", n);
+		n++;
+	}
 		while (n > 98)
-			printf("input value is :% d, ,") n--;
-		printf("%d\n", n);
-	}
-	else
-	{
-		while (n < 98)
-			printf("input value is : %d, ,") n++;
-		printf("%d\n", n);
-	}
+		{
+			printf("%i, ", n);
+			n--;
+		}
+
+		printf("98");
+		_putchar('\n');
 }
