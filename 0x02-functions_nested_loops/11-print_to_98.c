68a5e7b9c88ee2674f57576072a72cfe01308bef
LucyKanyua <lucy.munyange@gmail.com>
2023-02-22 02:22:19 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,17 +7,31 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98)
-	{
-		printf("%i, ", n);
-		n++;
-	}
-		while (n > 98)
-		{
-			printf("%i, ", n);
-			n--;
-		}
-
-		printf("98");
-		_putchar('\n');
+if (n < 98)
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+}
+}
+}
 }
