aa2b4213a002015ab3ec1cad73382043d9ec5cde
LucyKanyua <lucy.munyange@gmail.com>
2023-02-23 12:39:06 -0800

    2345

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,11 @@
-hgh
+#include "main.h"
+/**
+ * mul - function that multiplies
+ * @a: first input
+ * @b: second input
+ * Return: multiplication result
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
