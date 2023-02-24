578eebe282d44b539df39562b6fcfe8b2cc79770
LucyKanyua <lucy.munyange@gmail.com>
2023-02-24 01:12:01 -0800

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * file - main.h
+ * description: header contains prototypes for all the functions
+ * in the 0x04-more_functions_nested_loops project
+ */
+
 int _isupper(int c)
 int _isdigit(int c)
 int mul(int a, int b)
