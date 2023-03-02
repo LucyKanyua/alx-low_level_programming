f699766fcbd85d1d0665fd036eaf823da7019c1c
LucyKanyua <lucy.munyange@gmail.com>
2023-03-02 05:29:10 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,9 @@
-#include "main.h"
 #include <unistd.h>
-
 /**
  * _putchar - writes character c to stdout
  * @c: the character to print
  * Return: on success 1
- * on error , return -1 and errno is set appropriately
+ * on error , return -1
  */
 int _putchar(char c)
 {
