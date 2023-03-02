c44e3c022c2a931e29441bc384bd729413dbf5c7
LucyKanyua <lucy.munyange@gmail.com>
2023-03-02 00:10:44 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * strcat - concatenates the string pointed to by @src including the terminat
+ * _strcat - concatenates the string pointed to by @src including the terminat
  *	    null byte, to the end of the string pointed to by @dest
  * @dest: a pointer to the string to be concatenated upon
  * @src: the source string to be appended to @dest
