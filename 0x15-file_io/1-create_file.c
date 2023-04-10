0f98e470676ba9dd55a8165c5d33ad4112acfe83
LucyKanyua <lucy.munyange@gmail.com>
2023-04-10 01:41:42 -0700

    add some changes to 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,14 +11,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fq;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	fq = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, strlen(text_content));
-	close(fd);
+
+	write(fq, text_content, strlen(text_content));
+
+	close(fq);
+
 	return (1);
 }
