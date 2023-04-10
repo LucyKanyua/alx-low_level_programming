23fbe6f8e692161c2bf5816e371f6e346c4bab0e
LucyKanyua <lucy.munyange@gmail.com>
2023-04-10 00:53:49 -0700

    add some changes 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,8 +19,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (text_content != NULL)
 		w = write(fd, text_content, strlen(text_content));
-	if (fd == -1 || w == -1)
-		return (-1);
 	close(fd);
 	return (1);
 }
