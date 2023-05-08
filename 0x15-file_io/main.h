b2242675a07bab94e3d82d08086da28fb673c2d0
root <lucy.munyange@gmail.com>
2023-05-08 08:42:20 -0700

    added some modifications to main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_h
+#define MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -11,4 +11,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 
-#endif
+#endif /* MAIN_H */
