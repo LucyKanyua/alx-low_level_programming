bba025466d1f06000cdeede69c4bf166a327d590
root <lucy.munyange@gmail.com>
2023-06-15 11:57:37 -0700

    added modifications for the betty check

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,9 +11,9 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", cur->n);
+		printf("%d\n", h->n);
 		count++;
-		cur = cur->next;
+		h = h->next;
 	}
 	return (count);
 }
